68ee585c23b1d0d3abfc3d68bb3f07f6f74544c3
Iftikhar Shams Niloy <81116328+Iftikhar-Shams-Niloy@users.noreply.github.com>
2025-09-11 18:31:00 +0600

    update

--- lib/core/Theme/app_theme.dart
+++ lib/core/Theme/app_theme.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_lakshman1020/core/constants/app_colors.dart';
-import 'package:google_fonts/google_fonts.dart';
 
+import 'package:google_fonts/google_fonts.dart';
 
 class AppTheme {
   static ThemeData get light => ThemeData(
--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -15,8 +15,6 @@ class TColors {
   static const Color grey1 = Color(0xFF555659);
   static const Color grey2 = Color(0xFF6C6E72);
   static const Color deliveryDetails = Color(0xFF18191A); //nav bar text color
-  static const Color grey2 = Color(0xFF6C6E72); 
-  static const Color deliveryDetails = Color(0xFF18191A);//nav bar text color          
 
   static const Color account = Color(0xFFFCFDFF); //Text color
   static const Color personalBackground = Color(0xFFDCE4F5);
