e4e0ba53042903622a3aebcb3fc5a7a5c94d1bda
Iftikhar Shams Niloy <niloyshams21@gmail.com>
2025-10-26 17:09:34 +0600

    Necessary changes in auth

--- lib/features/auth/users/data/model/forgot_pass_response_model.dart
+++ lib/features/auth/users/data/model/forgot_pass_response_model.dart
@@ -1,29 +1,3 @@
-// class ForgotPassResponseModel {
-//   final bool success;
-//   final String message;
-//   final dynamic data; // 👈 add this if you need to keep 'data' (optional)
-
-//   ForgotPassResponseModel({
-//     required this.success,
-//     required this.message,
-//     this.data,
-//   });
-
-//   factory ForgotPassResponseModel.fromJson(Map<String, dynamic> json) {
-//     // Handle if 'data' is a string, null, or map
-//     final dataField = json['data'];
-
-//     return ForgotPassResponseModel(
-//       success: json['success'] ?? false,
-//       message: json['message'] ?? '',
-//       data: (dataField is Map<String, dynamic> || dataField is List)
-//           ? dataField
-//           : null, // prevent 'String' from causing errors
-//     );
-//   }
-// }
-
-
 class ForgotPassResponseModel {
   final bool success;
   final String message;
